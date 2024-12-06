9bbdd57d80e9e6b83c2810425d02542ce383acdb
Laurent Ouma <lawiomosh3@gmail.com>
2024-12-06 11:15:44 +0300

    used static data and improoved ui for settings screens

--- packages/core-components/src/components/SectionCard/SectionCard.tsx
+++ packages/core-components/src/components/SectionCard/SectionCard.tsx
@@ -3,15 +3,17 @@ import React, { FC, PropsWithChildren } from "react";
 import { StyleSheet } from "react-native";
 
 type Props = PropsWithChildren<{
-  title: string;
+  title?: string;
 }>;
 
 const SectionCard: FC<Props> = ({ title, children }) => {
   return (
     <Box backgroundColor={"skeleton"} p={"m"} borderRadius={"large"} gap={"s"}>
-      <Text variant={"titleMedium"} color={"text"} fontWeight={"700"}>
-        {title}
-      </Text>
+      {title && (
+        <Text variant={"titleMedium"} color={"text"} fontWeight={"700"}>
+          {title}
+        </Text>
+      )}
       {children}
     </Box>
   );
--- packages/features-settings/src/screens/SettingsScreen.tsx
+++ packages/features-settings/src/screens/SettingsScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 
 import {
   AppBar,
@@ -14,31 +14,62 @@ import { ThemeTogglerSection } from "../widgets";
 export const SettingsScreen = () => (
   <StyledPageLayout>
     <AppBar title="Settings" leading={false} />
-    <Box p={"m"} flex={1} flexDirection={"column"} gap={"m"}>
-      <SectionCard title="Account">
-        <ListTile
-          title="User"
-          subtitle="subtitle"
-          trailing={
-            <ExpoIconComponent
-              family="MaterialCommunityIcons"
-              name="chevron-right"
-            />
-          }
-        />
-        <ListTile
-          title="User"
-          subtitle="subtitle"
-          trailing={
-            <ExpoIconComponent
-              family="MaterialCommunityIcons"
-              name="chevron-right"
-            />
-          }
-        />
-      </SectionCard>
-      <ThemeTogglerSection />
-    </Box>
+    <ScrollView>
+      <Box p={"m"} flex={1} flexDirection={"column"} gap={"m"}>
+        <SectionCard title="Account">
+          <ListTile
+            title="Laurent Ouma"
+            subtitle="lawiomosh3@gmail.com"
+            leading={<ExpoIconComponent family="Feather" name="user" />}
+            trailing={
+              <ExpoIconComponent
+                family="MaterialCommunityIcons"
+                name="chevron-right"
+              />
+            }
+          />
+          <ListTile
+            title="Platven LTD"
+            subtitle="Tap to change organization scope"
+            leading={
+              <ExpoIconComponent family="SimpleLineIcons" name="organization" />
+            }
+            trailing={
+              <ExpoIconComponent
+                family="MaterialCommunityIcons"
+                name="chevron-right"
+              />
+            }
+          />
+          <ListTile
+            title="Change password"
+            leading={<ExpoIconComponent family="Feather" name="key" />}
+            subtitle="Use string secure password"
+            trailing={
+              <ExpoIconComponent
+                family="MaterialCommunityIcons"
+                name="chevron-right"
+              />
+            }
+          />
+        </SectionCard>
+
+        <ThemeTogglerSection />
+        <SectionCard>
+          <ListTile
+            leading={<ExpoIconComponent family="AntDesign" name="logout" />}
+            title="logout"
+            subtitle="End current session"
+            trailing={
+              <ExpoIconComponent
+                family="MaterialCommunityIcons"
+                name="chevron-right"
+              />
+            }
+          />
+        </SectionCard>
+      </Box>
+    </ScrollView>
   </StyledPageLayout>
 );
 
